1f527e07ecf7d624f804c109353c78792967fcb3
aaditya1606 <aadityashakya.tech@yahoo.com>
2026-07-24 12:45:59 +0530

    PRINT TABLE OF A GIVEN NUMBER

---
+++ print_table.c
@@ -0,0 +1,17 @@
+/*
+PROGRAM: PRINT TABLE OF A GIVEN NUMBER
+LANGUAGE: C
+AUTHOR- AADITYA SHAKYA
+*/
+#include <stdio.h>
+int main()
+{
+    int n, i;
+    printf("Enter the number: ");
+    scanf("%d", &n);
+    for (i = 1; i < 11; i++)
+    {
+        printf("%d x %d = %d\n", n, i, n * i);
+    }
+    return 0;
+}
