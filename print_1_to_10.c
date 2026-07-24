771fce16cea390645e381daca4b04211416e208d
aaditya1606 <aadityashakya.tech@yahoo.com>
2026-07-24 12:24:30 +0530

    PRINT 1 TO 10

---
+++ print_1_to_10.c
@@ -0,0 +1,17 @@
+/*
+PROGRAM: PRINT NUMBERS FROM 1 TO 10
+LANGUAGE: C
+AUTHOR- AADITYA SHAKYA
+*/
+#include <stdio.h>
+int main()
+{
+    int i = 1;
+    printf("SEQUENCE: ");
+    while (i < 11)
+    {
+        printf("%d ", i);
+        i++;
+    }
+    return 0;
+}
